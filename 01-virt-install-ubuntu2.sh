6c8f966e5cb233caca239003578b6b8f51a5f87a
datawolf <wanglong@cse.buaa.edu.cn>
2013-06-02 10:22:25 +0800

    install ubuntu 11.10 server

--- 01-virt-install-ubuntu2.sh
+++ 01-virt-install-ubuntu2.sh
@@ -3,7 +3,7 @@ virt-install \
 --ram 512 \
 --vcpus 2 \
 --disk /var/lib/libvirt/images/ubuntu2.img,size=8 \
---cdrom /var/lib/libvirt/images/ubuntu-13.04-server-i386.iso \
+--cdrom /var/lib/libvirt/images/ubuntu-11.10-server-i386.iso \
 --network bridge=br0  \
 --vnc \
 --noautoconsole \
